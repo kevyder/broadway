e0d729e6e216647c179f3cfebf4166e890121285
Kevin <kyrodriguez97@gmail.com>
2020-07-05 17:52:51 -0500

    Create new play action

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
 class PlaysController < ApplicationController
-  def index
+  def index; end
+
+  def new
+    @play = Play.new
+  end
+
+  def create
+    @play = Play.new(play_params)
+  end
+
+  private
+
+  def play_params
+    params.require(:play).permit(:title, :description, :director)
   end
 end
