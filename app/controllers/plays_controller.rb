f8ce12c2291be218114420b9f4ec41969cd0a4ce
Kevin <kyrodriguez97@gmail.com>
2020-07-23 20:42:14 -0500

    Play image

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -46,7 +46,7 @@ class PlaysController < ApplicationController
   private
 
   def play_params
-    params.require(:play).permit(:title, :description, :director, :category_id)
+    params.require(:play).permit(:title, :description, :director, :image, :category_id)
   end
 
   def find_play
